6256b312be9cdc4188e8ab8acfda412ac3219b87
DVE <celine.dv@hotmail.fr>
2021-04-22 16:49:09 +0200

    Les routes fonctionnent, reste like et dislike.

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,24 +2,57 @@ const Sauce = require("../models/sauce");
 const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-      delete req.body._id;
-      const sauce = new Sauce({
-        userId: req.body.userId,
-        name: req.body.name,
-        manufacturer: req.body.manufacturer,
-        description: req.body.description,
-        mainPepper: req.body.mainPepper,
-        imageUrl: req.body.imageUrl,
-        heat: req.body.heat,
-        likes: 0,
-        dislikes: 0,
-        usersLiked: [],
-        usersDisliked: [],
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
+  });
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Sauce enregistrée !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.modifySauce = (req, res, next) => {
+  const sauceObject = req.file
+    ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+      }
+    : { ...req.body };
+  Sauce
+    .updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
+
+exports.deleteSauce = (req, res, next) => {
+  Sauce
+    .findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce
+            .deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: "Sauce supprimé !" }))
+            .catch((error) => res.status(400).json({ error }));
       });
-      sauce
-        .save()
-        .then(() => res.status(201).json ({ message: 'Objet enregistré !'}))
-        .catch(error => res.status(400).json ({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+  Sauce
+    .findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(404).json({ error }));
 };
 
 exports.getAllSauces = (req, res, next) => {
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,7 +5,10 @@ const auth = require('../middleware/auth');
 const sauceCtrl = require('../controllers/sauce');
 const multer = require("../middleware/multer-config");
 
-router.post('/', auth, sauceCtrl.createSauce);
+router.post("/", auth, multer, sauceCtrl.createSauce);
+router.put("/:id", auth, multer, sauceCtrl.modifySauce);
+router.delete("/:id", auth, sauceCtrl.deleteSauce);
+router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
 
 module.exports = router;
