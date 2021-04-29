ad67993ff0b7a1db9c9be0da0c2b4e818b2dc320
DVE <celine.dv@hotmail.fr>
2021-04-29 08:28:37 +0200

    like dislike fonctionne

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -5,10 +5,11 @@ const auth = require('../middleware/auth');
 const sauceCtrl = require('../controllers/sauce');
 const multer = require("../middleware/multer-config");
 
-router.post("/", auth, multer, sauceCtrl.createSauce);
-router.put("/:id", auth, multer, sauceCtrl.modifySauce);
-router.delete("/:id", auth, sauceCtrl.deleteSauce);
-router.get("/:id", auth, sauceCtrl.getOneSauce);
-router.get('/', auth, sauceCtrl.getAllSauces);
+router.post("/", auth, multer, sauceCtrl.createSauce); /* ajout d'un élément */
+router.put("/:id", auth, multer, sauceCtrl.modifySauce); /* modification d'un élément */
+router.delete("/:id", auth, sauceCtrl.deleteSauce); /* supprimer un élément */
+router.get("/:id", auth, sauceCtrl.getOneSauce); /* Retourne un seul élément par son ID */
+router.get('/', auth, sauceCtrl.getAllSauces); /* Retourne toutes les éléments */
+router.post('/:id/like', auth, sauceCtrl.sauceLike);
 
 module.exports = router;
