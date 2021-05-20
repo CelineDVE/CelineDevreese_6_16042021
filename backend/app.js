afe40c8c08b1d7debe9ac11c6bb16a7ed1e9acfd
DVE <celine.dv@hotmail.fr>
2021-05-20 06:02:37 +0200

    final commit

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,7 @@ app.use(bodyParser.json());
 app.use(xss());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(express.static("images"));
 
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
